5282f601b264045220e85339890ce782bbe7d035
max_wang <405323395@qq.com>
2017-07-20 01:52:01 +0800

    增加模拟数据

--- data/data.js
+++ data/data.js
@@ -18,6 +18,67 @@ var data = {
 },
   //-------------------------------------------
   xianshimiaosha: [{
+
+    'id': 1001,   //优惠券id
+    'relaId': 1253,//优惠券挂载到人员的id。
+    'imgUrl': '../../img/product5.png',//商铺图片
+    'storeName': '小店',//商铺名称
+    'couponDescrip': '全场买二送一',
+    'useCondition': '新品除外'
+
+  }, {
+
+    'id': 1001,   //优惠券id
+    'relaId': 1253,//优惠券挂载到人员的id。
+    'imgUrl': '../../img/product5.png',//商铺图片
+    'storeName': '小店',//商铺名称
+    'couponDescrip': '全场买二送一',
+    'useCondition': '新品除外'
+
+  },{
+    'id': 1001,   //优惠券id
+    'relaId': 12451,//优惠券挂载到人员的id。
+    'imgUrl': '../../img/product4.png',//商铺图片
+    'storeName': '小店',//商铺名称
+    'couponDescrip': '特价水果8.8折',
+    'useCondition': '新品除外'
+  }, {
+    'id': 1001,   //优惠券id
+    'relaId': 12451,//优惠券挂载到人员的id。
+    'imgUrl': '../../img/product3.png',//商铺图片
+    'storeName': '小店',//商铺名称
+    'couponDescrip': '特价水果8.8折',
+    'useCondition': '新品除外'
+  }, {
+    'id': 1001,   //优惠券id
+    'relaId': 12451,//优惠券挂载到人员的id。
+    'imgUrl': '../../img/product2.png',//商铺图片
+    'storeName': '小店',//商铺名称
+    'couponDescrip': '特价水果8.8折',
+    'useCondition': '新品除外'
+  }, {
+    'id': 1001,   //优惠券id
+    'relaId': 1253,//优惠券挂载到人员的id。
+    'imgUrl': '../../img/product1.png',//商铺图片
+    'storeName': '小店',//商铺名称
+    'couponDescrip': '五元现金券免费送',
+    'useCondition': '新品除外'
+
+  },{
+    'id': 1001,   //优惠券id
+    'relaId': 12451,//优惠券挂载到人员的id。
+    'imgUrl': '../../img/product4.png',//商铺图片
+    'storeName': '小店',//商铺名称
+    'couponDescrip': '特价水果8.8折',
+    'useCondition': '新品除外'
+  }, {
+    'id': 1001,   //优惠券id
+    'relaId': 12451,//优惠券挂载到人员的id。
+    'imgUrl': '../../img/product3.png',//商铺图片
+    'storeName': '小店',//商铺名称
+    'couponDescrip': '特价水果8.8折',
+    'useCondition': '新品除外'
+  },{
       'id': 1001,   //优惠券id
       'relaId':12451,//优惠券挂载到人员的id。
       'imgUrl':'../../img/product2.png',//商铺图片
@@ -52,6 +113,54 @@ var data = {
       'couponDescrip': '全场买二送一',
       'useCondition': '新品除外'
 
+  }, {
+    'id': 1001,   //优惠券id
+    'relaId': 12451,//优惠券挂载到人员的id。
+    'imgUrl': '../../img/product2.png',//商铺图片
+    'storeName': '小店',//商铺名称
+    'couponDescrip': '特价水果8.8折',
+    'useCondition': '新品除外'
+  }, {
+    'id': 1001,   //优惠券id
+    'relaId': 1253,//优惠券挂载到人员的id。
+    'imgUrl': '../../img/product1.png',//商铺图片
+    'storeName': '小店',//商铺名称
+    'couponDescrip': '五元现金券免费送',
+    'useCondition': '新品除外'
+
+  }, {
+
+    'id': 1001,   //优惠券id
+    'relaId': 1253,//优惠券挂载到人员的id。
+    'imgUrl': '../../img/product4.png',//商铺图片
+    'storeName': '小店',//商铺名称
+    'couponDescrip': '全场买二送一',
+    'useCondition': '新品除外'
+
+  }, {
+    'id': 1001,   //优惠券id
+    'relaId': 12451,//优惠券挂载到人员的id。
+    'imgUrl': '../../img/product2.png',//商铺图片
+    'storeName': '小店',//商铺名称
+    'couponDescrip': '特价水果8.8折',
+    'useCondition': '新品除外'
+  }, {
+    'id': 1001,   //优惠券id
+    'relaId': 1253,//优惠券挂载到人员的id。
+    'imgUrl': '../../img/product1.png',//商铺图片
+    'storeName': '小店',//商铺名称
+    'couponDescrip': '五元现金券免费送',
+    'useCondition': '新品除外'
+
+  }, {
+
+    'id': 1001,   //优惠券id
+    'relaId': 1253,//优惠券挂载到人员的id。
+    'imgUrl': '../../img/product4.png',//商铺图片
+    'storeName': '小店',//商铺名称
+    'couponDescrip': '全场买二送一',
+    'useCondition': '新品除外'
+
   }],
   //-------------------------------------------
   wubaimishangquan: [{
@@ -72,7 +181,31 @@ var data = {
       'couponDescrip': '全场买二送一',
       'useCondition': '新品除外'
 
-  }],
+    }, {
+      'id': 1001,   //优惠券id
+      'relaId': 12451,//优惠券挂载到人员的id。
+      'imgUrl': '../../img/product2.png',//商铺图片
+      'storeName': '小店',//商铺名称
+      'couponDescrip': '特价水果8.8折',
+      'useCondition': '新品除外'
+    }, {
+      'id': 1001,   //优惠券id
+      'relaId': 1253,//优惠券挂载到人员的id。
+      'imgUrl': '../../img/product1.png',//商铺图片
+      'storeName': '小店',//商铺名称
+      'couponDescrip': '五元现金券免费送',
+      'useCondition': '新品除外'
+
+    }, {
+
+      'id': 1001,   //优惠券id
+      'relaId': 1253,//优惠券挂载到人员的id。
+      'imgUrl': '../../img/product4.png',//商铺图片
+      'storeName': '小店',//商铺名称
+      'couponDescrip': '全场买二送一',
+      'useCondition': '新品除外'
+
+    }],
   //-------------------------------------------
   pingpairuzhu: [{
  
@@ -82,6 +215,30 @@ var data = {
       'storeName': '小店',//商铺名称
       'couponDescrip': '全场买二送一',
       'useCondition': '新品除外'
+
+  }, {
+    'id': 1001,   //优惠券id
+    'relaId': 12451,//优惠券挂载到人员的id。
+    'imgUrl': '../../img/product2.png',//商铺图片
+    'storeName': '小店',//商铺名称
+    'couponDescrip': '特价水果8.8折',
+    'useCondition': '新品除外'
+  }, {
+    'id': 1001,   //优惠券id
+    'relaId': 1253,//优惠券挂载到人员的id。
+    'imgUrl': '../../img/product1.png',//商铺图片
+    'storeName': '小店',//商铺名称
+    'couponDescrip': '五元现金券免费送',
+    'useCondition': '新品除外'
+
+  }, {
+
+    'id': 1001,   //优惠券id
+    'relaId': 1253,//优惠券挂载到人员的id。
+    'imgUrl': '../../img/product4.png',//商铺图片
+    'storeName': '小店',//商铺名称
+    'couponDescrip': '全场买二送一',
+    'useCondition': '新品除外'
 
   }]
 
