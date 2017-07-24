b983132ede3e090d8a0bef9906f9d48cbb23177f
max_wang <405323395@qq.com>
2017-07-25 00:16:09 +0800

    红包界面

--- pages/redPackage/red.js
+++ pages/redPackage/red.js
@@ -5,14 +5,43 @@ Page({
    * 页面的初始数据
    */
   data: {
-  
+    animation:'',
+    animation_pc1:'',
+    animation_pc2: '',
+    animation_pc3: '',
+    animation_pc4: ''
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
+    this.animation_pc1 = wx.createAnimation({
+      duration: 200, timingFunction: 'ease-out', delay: 100
+    });
+      this.animation_pc2 = wx.createAnimation({
+        duration: 200, timingFunction: 'ease-out', delay: 100
+      });
+    this.animation_pc3 = wx.createAnimation({
+      duration: 200, timingFunction: 'ease-out', delay: 100
+    });
+    this.animation_pc4 = wx.createAnimation({
+      duration: 200, timingFunction: 'ease-out', delay: 100
+    });
+    this.animation = wx.createAnimation({
+      transformOrigin: 'left top 0',
+      success: function (res) {
+        console.log(res)
+      }
+    })
+  },
+  coinClick:function(e){
+    this.animation.translateY(-45).step({ duration: 200, timingFunction: 'ease-out', delay: 100})
+    this.animation.translateY(400).step({ duration: 400, delay: 150, timingFunction:'ease-in' })
+    this.setData({
+      animation: this.animation.export()
+    })
+
   },
 
   /**
@@ -26,7 +55,16 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-  
+    this.animation_pc1.translateY(400).step({ duration: 200, timingFunction: 'ease-out', delay: 1000 });
+    this.animation_pc2.translateY(400).step({ duration: 400, timingFunction: 'ease-in', delay: 3000, });
+    this.animation_pc3.translateY(400).step({ duration: 200, timingFunction: 'ease-out', delay: 4000 });
+    this.animation_pc4.translateY(400).step({ duration: 400, timingFunction: 'ease-in', delay: 500, });
+    this.setData({
+      animation_pc1: this.animation_pc1.export(),
+      animation_pc2: this.animation_pc2.export(),
+      animation_pc3: this.animation_pc3.export(),
+      animation_pc4: this.animation_pc4.export(),
+    })
   },
 
   /**
