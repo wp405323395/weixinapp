2efdf4b3d0296dd99692defdd1cffded259912eb
max_wang <405323395@qq.com>
2017-07-31 16:50:42 +0800

    我是商家模块

--- pages/business/business.js
+++ pages/business/business.js
@@ -1,66 +1,125 @@
 // business.js
+var util = require('../../utils/util.js')
+var formatLocation = util.formatLocation
+var sourceType = [['camera'], ['album'], ['camera', 'album']]
+var sizeType = [['compressed'], ['original'], ['compressed', 'original']]
+var longClick;
+var longClick2;
+var imgList;
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-  
-  },
+    imageList: [],
+    sourceTypeIndex: 2,
+    sourceType: ['拍照', '相册', '拍照或相册'],
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-  
-  },
+    sizeTypeIndex: 2,
+    sizeType: ['压缩', '原图', '压缩或原图'],
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-  
+    countIndex: 2,
+    count: [1, 2, 3, 4, 5, 6, 7, 8, 9],
+    isHidden_delete:true,
+    hasLocation:false
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-  
+  showDelete:function(e){
+    this.setData({
+      isHidden_delete: false
+    })
+    longClick = true;
+    longClick2 = true;
+    
   },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-  
+  sourceTypeChange: function (e) {
+    this.setData({
+      sourceTypeIndex: e.detail.value
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-  
+  sizeTypeChange: function (e) {
+    this.setData({
+      sizeTypeIndex: e.detail.value
+    })
   },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-  
+  countChange: function (e) {
+    this.setData({
+      countIndex: e.detail.value
+    })
   },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-  
+  chooseImage: function () {
+    var that = this
+    if (this.data.imageList.length - 1 >= this.data.countIndex) {
+      return ;
+    }
+    imgList = this.data.imageList;
+    wx.chooseImage({
+      sourceType: sourceType[this.data.sourceTypeIndex],
+      sizeType: sizeType[this.data.sizeTypeIndex],
+      count: this.data.count[this.data.countIndex],
+      success: function (res) {
+        console.log(res)
+        imgList = imgList.concat(res.tempFilePaths);
+        that.setData({
+          imageList: imgList
+        })
+      }
+    })
   },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-  
+  previewImage: function (e) {
+    console.log("previewImage");
+    if (longClick){
+      longClick = false;
+      return ;
+    } else {
+      this.setData({
+        isHidden_delete: true
+      })
+      var current = e.target.dataset.src
+      wx.previewImage({
+        current: current,
+        urls: this.data.imageList
+      })
+    }
+    
+  }, 
+  onChooceLocation: function(e) {
+    var that = this;
+    wx.chooseLocation({
+      success: function (res) {
+        console.log(res)
+        that.setData({
+          hasLocation: true,
+          location: formatLocation(res.longitude, res.latitude),
+          locationAddress: res.address
+        })
+      }
+    });
+  },
+  deleteItem:function(e) {
+    var current = e.target.dataset.src;
+    var index = this.data.imageList.indexOf(current);
+    this.data.imageList.splice(index, 1);
+    this.setData({
+      imageList: this.data.imageList
+    })
+  },
+  on_addPic_wrap_click:function(e){
+    if (longClick2) {
+      longClick2 = false;
+      return;
+    } else {
+      this.setData({
+        isHidden_delete: true
+      })
+    }
+    
+  },
+  onKindClick:function(e) {
+    let id = e.currentTarget.dataset.kindid;
+    this.setData({
+      type_index: id
+    })
+    console.log(e);
+  },
+  submit:function(e){
+    console.log("----dddddddddddd");
   }
+
 })
