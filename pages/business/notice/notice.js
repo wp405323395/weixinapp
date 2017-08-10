218431c9d87175bb2e7a5419eab18a934ab209c8
max_wang <405323395@qq.com>
2017-08-10 22:55:49 +0800

    优化界面路基

--- pages/business/notice/notice.js
+++ pages/business/notice/notice.js
@@ -2,34 +2,39 @@
 //var time1 = new Date().format("yyyy-MM-dd HH:mm:ss");   
 var noticeList = [{
   id: '100',
-  submitTime: '2017-8-10  02:32:34',
+  submitTime: '2017-8-10  15:32:34',
   coupName: '新鲜水果',
   noticeStr: '已经成功发布，正在审核中',
-  coupStatus: '0'//0:审核中，1:审核通过,2:未通过,3:过期.
+  coupStatus: '0',//0:审核中，1:审核通过,2:未通过,3:过期.
+  isHasRead:false
 },{
   id:'100',
   submitTime:'2017-01-20  02:32:34',
   coupName:'新鲜水果',
   noticeStr:'已经成功发布，正在审核中',
-  coupStatus:'0'//0:审核中，1:审核通过,2:未通过,3:过期.
+  coupStatus:'0',//0:审核中，1:审核通过,2:未通过,3:过期.
+  isHasRead: true
 },{
     id: '100',
     submitTime: '2017-01-20 12:32:20',
     coupName: '新鲜水果',
     noticeStr: '已经成功审核',
-    coupStatus: '1'
+    coupStatus: '1',
+    isHasRead: false
   }, {
     id: '100',
     submitTime: '2017-01-16 22:32:16',
     coupName: '新鲜水果',
     noticeStr: '未通过审核',
-    coupStatus: '2'
+    coupStatus: '2',
+    isHasRead: false
 }, {
     id: '100',
   submitTime: '2017-01-2 18:32:49',
   coupName: '新鲜水果',
   noticeStr: '已过期',
-  coupStatus: '3'
+  coupStatus: '3',
+  isHasRead: false
 }]
 Page({
 
@@ -143,6 +148,11 @@ Page({
   },
   onItemClick:function(e){
     let coupId = e.currentTarget.dataset.coupId;
+    let index = e.currentTarget.dataset.index;
+    noticeList[index].isHasRead = true;
+    this.setData({
+      noticeList: noticeList
+    });
     wx.navigateTo({
       url: '../coupReview/coupReview?id='+coupId,
     })
