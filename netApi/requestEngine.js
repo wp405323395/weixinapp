63c7f10786608e48c0ca24d2233bb654bee7859f
max_wang <405323395@qq.com>
2017-08-13 18:56:57 +0800

    增加注释。

--- netApi/requestEngine.js
+++ netApi/requestEngine.js
@@ -8,8 +8,16 @@ class RequestEngine {
   addInterceptor(interceptor) {
     this.interceptors.push(interceptor);
   }
-
+  /**
+   * url:请求路径。
+   * data：请求参数
+   * reqMethod:失败后的重试函数。
+   * requestSuccess:请求成功的回调。
+   * requestFail:请求失败的回调。
+   * reqestComplete:请求完成的回调。
+   */
   request(url, data, reqMethod, requestSuccess, requestFail, requestComplete) {
+    //此处添加拦截器。
     let logInterceptor = new LogInterceptor();
     this.addInterceptor(logInterceptor);
      requestModle.request(url, data, reqMethod, requestSuccess, requestFail, requestComplete, this.interceptors);
