aa30fc879bda0658483f05c40717465e56c8ada4
max_wang <405323395@qq.com>
2017-08-15 12:49:22 +0800

    增加页面进入的延迟请求处理

--- pages/business/totalCoup/coups.js
+++ pages/business/totalCoup/coups.js
@@ -35,7 +35,10 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    this.loadAllCoup();
+    setTimeout(()=>{
+      this.loadAllCoup();
+    },500)
+    
   },
   loadAllCoup:function(e){
     let that = this;
