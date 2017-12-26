9d018efb59bd73a14e8d571baa8e1e59107f3275
max_wang <405323395@qq.com>
2017-12-26 17:31:46 +0800

    项目进度

--- config.js
+++ config.js
@@ -7,9 +7,9 @@
 
 
 var host = 'www.maywidehb.com';
-//var host = "192.168.1.199:8015"
+//var host = "192.168.1.166:8015"
 var isHttps = (host == 'www.maywidehb.com')?true:false;
-var isDebug = false;
+var isDebug = true;
 var schema = isHttps ? 'https':'http';
 var srcHost = 'https://www.maywidehb.com/'
 var config = {
---
+++ pages/yuandanproject3/ad/ad.js
@@ -0,0 +1,110 @@
+// pages/yuandanproject/contribution/contribution.js
+import RequestEngine from '../../../netApi/requestEngine.js';
+var config = require('../../../config.js');
+var util = require('../../../utils/util.js');
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    ishiddenToast: true,
+    isDisable: true
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    let scene = util.getScene(options);
+    var relation = scene.split('~')[1];
+    var qrid = scene.split('~')[2];
+    this.relation = relation;
+    this.qrid = qrid;
+    setTimeout(() => {
+      this.setData({
+        ishiddenToast: false,
+        isDisable: false
+      });
+      setTimeout(() => {
+        this.setData({
+          ishiddenToast: true,
+          isDisable: false
+        })
+      }, 4000);
+
+    }, 3000);
+  },
+
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+    // wx.navigateTo({
+    //   url: '../postCoup/postCoup',
+    // })
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  },
+  gotoseeTv: function () {
+    let that = this;
+    wx.navigateTo({
+      url: '../../yuandanproject/freewatchtv/freewatchtv?qrid=' + that.qrid,
+    })
+  },
+  gotocontribution: function () {
+    let that = this;
+    wx.navigateToMiniProgram({
+      appId: 'wxce0945b006f54c3a',
+      path: 'pages/tvCardModel/tvCardIndex/tvcard?qrid=' + this.qrid,
+      extraData: {
+        foo: 'bar'
+      },
+      envVersion: 'develop',
+      success(res) {
+        // 打开成功
+      }
+    })
+  }
+})
