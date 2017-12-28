4b7a8967782f3d873b3fdb711cb54bdda8874a61
max_wang <405323395@qq.com>
2017-12-28 11:58:45 +0800

    项目进度

--- config.js
+++ config.js
@@ -7,7 +7,7 @@
 
 
 var host = 'www.maywidehb.com';
-//var host = "192.168.1.166:8015"
+var host = "192.168.1.181:8015"
 var isHttps = (host == 'www.maywidehb.com')?true:false;
 var isDebug = true;
 var schema = isHttps ? 'https':'http';
