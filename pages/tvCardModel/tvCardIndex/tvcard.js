16ad496d533a772139dd963fb83f7c8489ec1fdf
max_wang <405323395@qq.com>
2018-01-17 16:25:56 +0800

    项目进度

--- pages/tvCardModel/tvCardIndex/tvcard.js
+++ pages/tvCardModel/tvCardIndex/tvcard.js
@@ -110,7 +110,7 @@ Page({
       });
     }).then(value=>{
       let cardInfo = value.custList[0];
-      let url = '../pay/pay?tvCardNum=' + this.tvCardNum + '&custid=' + cardInfo.custid + '&serviceID=' + this.serviceID + "&qrKind=" + this.qrKind + "&addr=" + cardInfo.addr + "&custname=" + cardInfo.custname + "&mobile=" + cardInfo.mobile;
+      let url = '../pay/pay?tvCardNum=' + this.tvCardNum + '&custid=' + cardInfo.custid + '&serviceID=' + this.serviceID + "&qrKind=" + this.qrKind + "&addr=" + cardInfo.addr + "&custname=" + cardInfo.custname + "&mobile=" + cardInfo.mobile + "&city=" + cardInfo.city;
       wx.navigateTo({
         url: url
       })
