aa4d26657131ee1819342dbfd6a81f6ba38c854f
max_wang <405323395@qq.com>
2018-06-21 16:21:39 +0800

    小程序搬家

--- config.js
+++ config.js
@@ -5,8 +5,8 @@
 // 此处主机域名是腾讯云解决方案分配的域名
 // 小程序后台服务解决方案：https://www.qcloud.com/solution/la
 
-// var host = "192.168.2.107:8180"
 var host = 'www.htrnpay.cn';
+ //var host = "192.168.2.104:8383"
 var isHttps = ('www.htrnpay.cn' == host);
 var isDebug = !isHttps;
 var schema = isHttps ? 'https':'http';
@@ -16,50 +16,50 @@ var config = {
     isDebug,
     srcUrl: `${schema}://${host}/`,
     // 登录地址，用于建立会话
-    loginUrl: `${schema}://${host}/wappweb/qrwx/wx-order!login`,
-    discoverUrl: `${schema}://${host}/wappweb/mcwx/mc-wx!queryCouponList`,
-    mycoup: `${schema}://${host}/wappweb/mcwx/mc-wx!queryWxCouponList`,
-    loadProduct: `${schema}://${host}/wappweb/mcwx/mc-wx!queryCouponDetailById`,
-    useCoup: `${schema}://${host}/wappweb/mcwx/mc-wx!useCoupon`,
-    receiveCoup: `${schema}://${host}/wappweb/mcwx/mc-wx!receiveCoupon`,
-    received_tickit_url: `${schema}://${host}/wappweb/mcwx/mc-wx!queryUnuseCouponList`,
-    deleteCoup: `${schema}://${host}/wappweb/mcwx/mc-wx!delCoupon`,
+    loginUrl: `${schema}://${host}/scanweb/qrwx/wx-order!login`,
+    discoverUrl: `${schema}://${host}/scanweb/mcwx/mc-wx!queryCouponList`,
+    mycoup: `${schema}://${host}/scanweb/mcwx/mc-wx!queryWxCouponList`,
+    loadProduct: `${schema}://${host}/scanweb/mcwx/mc-wx!queryCouponDetailById`,
+    useCoup: `${schema}://${host}/scanweb/mcwx/mc-wx!useCoupon`,
+    receiveCoup: `${schema}://${host}/scanweb/mcwx/mc-wx!receiveCoupon`,
+    received_tickit_url: `${schema}://${host}/scanweb/mcwx/mc-wx!queryUnuseCouponList`,
+    deleteCoup: `${schema}://${host}/scanweb/mcwx/mc-wx!delCoupon`,
         
-    businessAuther: `${schema}://${host}/wappweb/mcwx/merc-wx!doMercSettled`,
-    queMercDetail: `${schema}://${host}/wappweb/mcwx/merc-wx!queMercDetail`,
-    publishMercCoup: `${schema}://${host}/wappweb/mcwx/merc-wx!publishMercCoup`,
+    businessAuther: `${schema}://${host}/scanweb/mcwx/merc-wx!doMercSettled`,
+    queMercDetail: `${schema}://${host}/scanweb/mcwx/merc-wx!queMercDetail`,
+    publishMercCoup: `${schema}://${host}/scanweb/mcwx/merc-wx!publishMercCoup`,
     
 
-    used_tickit_url: `${schema}://${host}/wappweb/mcwx/mc-wx!queryUsedCouponList`,
-    queryWxUserCouponDetail: `${schema}://${host}/wappweb/mcwx/mc-wx!queryWxUserCouponDetail`,
-    uploadBusinessPic: `${schema}://${host}/wappweb/mcwx/file-upload!doUpload`,
-    queMercSettled: `${schema}://${host}/wappweb/mcwx/merc-wx!queMercSettled`,
-    loadAllCoup: `${schema}://${host}/wappweb/mcwx/merc-wx!queMercCoupListByStoreid`,
+    used_tickit_url: `${schema}://${host}/scanweb/mcwx/mc-wx!queryUsedCouponList`,
+    queryWxUserCouponDetail: `${schema}://${host}/scanweb/mcwx/mc-wx!queryWxUserCouponDetail`,
+    uploadBusinessPic: `${schema}://${host}/scanweb/mcwx/file-upload!doUpload`,
+    queMercSettled: `${schema}://${host}/scanweb/mcwx/merc-wx!queMercSettled`,
+    loadAllCoup: `${schema}://${host}/scanweb/mcwx/merc-wx!queMercCoupListByStoreid`,
 
-    uploadQrInfo: `${schema}://${host}/wappweb/mcwx/mc-wx!saveCardidOpenid?scene=`,
-    useCouponByWxCode: `${schema}://${host}/wappweb/mcwx/mc-wx!useCouponByWxCode`,
-    mercSureUseCoup: `${schema}://${host}/wappweb/mcwx/merc-wx!mercSureUseCoup`,
-    queMercAssistListByStoreid: `${schema}://${host}/wappweb/mcwx/merc-wx!queMercAssistListByStoreid`,
+    uploadQrInfo: `${schema}://${host}/scanweb/mcwx/mc-wx!saveCardidOpenid?scene=`,
+    useCouponByWxCode: `${schema}://${host}/scanweb/mcwx/mc-wx!useCouponByWxCode`,
+    mercSureUseCoup: `${schema}://${host}/scanweb/mcwx/merc-wx!mercSureUseCoup`,
+    queMercAssistListByStoreid: `${schema}://${host}/scanweb/mcwx/merc-wx!queMercAssistListByStoreid`,
     
-    addStoreAssit: `${schema}://${host}/wappweb/mcwx/merc-wx!addStoreAssit`,
-    createStoreAssistCode: `${schema}://${host}/wappweb/mcwx/merc-wx!createStoreAssistCode`,
-    delAssistbyAssistid: `${schema}://${host}/wappweb/mcwx/merc-wx!delAssistbyAssistid`,
-    giveAwayCoupon: `${schema}://${host}/wappweb/mcwx/mc-wx!giveAwayCoupon`,
-    gainGiveAwayCoupon: `${schema}://${host}/wappweb/mcwx/mc-wx!gainGiveAwayCoupon`,
+    addStoreAssit: `${schema}://${host}/scanweb/mcwx/merc-wx!addStoreAssit`,
+    createStoreAssistCode: `${schema}://${host}/scanweb/mcwx/merc-wx!createStoreAssistCode`,
+    delAssistbyAssistid: `${schema}://${host}/scanweb/mcwx/merc-wx!delAssistbyAssistid`,
+    giveAwayCoupon: `${schema}://${host}/scanweb/mcwx/mc-wx!giveAwayCoupon`,
+    gainGiveAwayCoupon: `${schema}://${host}/scanweb/mcwx/mc-wx!gainGiveAwayCoupon`,
 
 
-    querySalesList: `${schema}://${host}/wappweb/qrwx/wx-order!querySalesList`,
-    queryCustInfo: `${schema}://${host}/wappweb/qrwx/wx-order!queryCustInfo`,
-    doOrder: `${schema}://${host}/wappweb/qrwx/wx-order!doOrder`,
-    wxPay: `${schema}://${host}/wappweb/qrwx/wx-order!wxPay`,
-    queryOrderKeyno: `${schema}://${host}/wappweb/qrwx/wx-order!queryOrderKeyno`,
-    queCustInfoByOpenid: `${schema}://${host}/wappweb/qrwx/wx-order!queCustInfoByOpenid`,
+    querySalesList: `${schema}://${host}/scanweb/qrwx/wx-order!querySalesList`,
+    queryCustInfo: `${schema}://${host}/scanweb/qrwx/wx-order!queryCustInfo`,
+    doOrder: `${schema}://${host}/scanweb/qrwx/wx-order!doOrder`,
+    wxPay: `${schema}://${host}/scanweb/qrwx/wx-order!wxPay`,
+    queryOrderKeyno: `${schema}://${host}/scanweb/qrwx/wx-order!queryOrderKeyno`,
+    queCustInfoByOpenid: `${schema}://${host}/scanweb/qrwx/wx-order!queCustInfoByOpenid`,
     
-    listCandidate: `${schema}://${host}/wappweb/vot/vot-wx!listCandidate`,
-    listWxQuestion: `${schema}://${host}/wappweb/vot/vot-wx!listWxQuestion`,
-    doVot: `${schema}://${host}/wappweb/vot/vot-wx!doVot`,
-    isHadVot: `${schema}://${host}/wappweb/vot/vot-wx!isHadVot`,
-    queQrcodePutById: `${schema}://${host}/wappweb/qrwx/qrcode!queQrcodePutById`,
+    listCandidate: `${schema}://${host}/scanweb/vot/vot-wx!listCandidate`,
+    listWxQuestion: `${schema}://${host}/scanweb/vot/vot-wx!listWxQuestion`,
+    doVot: `${schema}://${host}/scanweb/vot/vot-wx!doVot`,
+    isHadVot: `${schema}://${host}/scanweb/vot/vot-wx!isHadVot`,
+    queQrcodePutById: `${schema}://${host}/scanweb/qrwx/qrcode!queQrcodePutById`,
 };
 
 
