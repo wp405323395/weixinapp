e25ed561d56cdce70147a938039dbbdc2ddf4f3c
max_wang <405323395@qq.com>
2018-06-24 13:48:39 +0800

    项目进度

--- pages/index/liveTv/livetv.js
+++ pages/index/liveTv/livetv.js
@@ -29,9 +29,17 @@ Component({
         kindSelect:id
       });
       //todo: 切换频道类型，切换数据
+      console.log("开始切换频道类型");
     },
     addChannel:function(){
       //todo: 添加电视台
+      console.log("开始添加频道");
+      wx.navigateTo({
+        url: '../channelcenter/channelcenter',
+        success: function(res) {},
+        fail: function(res) {},
+        complete: function(res) {},
+      })
     },
     showDelete:function(){
       this.setData({
@@ -41,11 +49,12 @@ Component({
 
     despatchClick:function(e){
       if(e.target.id == "10105") {
+          let channelid = e.target.dataset.channelid;
           if(this.data.isShowDelete) {
-            console.log("开始删除应用");
+            console.log("开始删除频道 channelid= "+ channelid);
             //todo: 删除我的频道
           } else {
-            console.log("点击进入播放台");
+            console.log("点击进入播放台channelid= " + channelid);
             //todo: 播放频道
           }
       } else {
--- pages/index/topiczone/topiczone.js
+++ pages/index/topiczone/topiczone.js
@@ -18,6 +18,15 @@ Component({
    * 组件的方法列表
    */
   methods: {
-
+    gotoTopic:function(e) {
+      let topicId = e.target.dataset.topicid;
+      console.log("进入专题 id = "+ topicId);
+      wx.navigateTo({
+        url: '../topic/topicdetail/topicdetail?topicId='+topicId,
+        success: function(res) {},
+        fail: function(res) {},
+        complete: function(res) {},
+      })
+    }
   }
 })
