4fa68ff09573434bf6220120767f6989677099ba
max_wang <405323395@qq.com>
2018-06-29 16:17:49 +0800

    项目进度

--- pages/me/me.js
+++ pages/me/me.js
@@ -6,7 +6,11 @@ Page({
    * 页面的初始数据
    */
   data: {
-  
+    hasUserInfo: false,
+    canIUse: wx.canIUse('button.open-type.getUserInfo'),
+    userInfo:{
+      avatarUrl:"../../img/default_header.jpg"
+    }
   },
 
   /**
