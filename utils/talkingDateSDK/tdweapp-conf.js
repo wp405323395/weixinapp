a2c82fd7f8e44d15524fbb9473d32a119032e7c2
max_wang <405323395@qq.com>
2018-07-17 15:47:10 +0800

    添加行为统计

---
+++ utils/talkingDateSDK/tdweapp-conf.js
@@ -0,0 +1,11 @@
+exports.config = {
+  appkey: '210863C3DF83421DB6CA388E226A0D71',
+  appName: '视家tv',
+  versionName: '初始上线版本',
+  versionCode: '1.0.0',
+  wxAppid: 'wxf905f7b3d14f0351',
+  getUserInfo: false, // 默认不获取用户信息
+  getLocation: false, // 默认不获取用户位置
+  autoOnPullDownRefresh: true, // 默认不统计下拉刷新数据
+  autoOnReachBottom: true // 默认不统计页面触底数据
+};
