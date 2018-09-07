c0d02ab33d3df547a3b32179abacdffea00ad045
max_wang <405323395@qq.com>
2018-09-07 16:28:00 +0800

    整理代码第一步

---
+++ pages/tvCardModel/requestUtil/netData.js
@@ -0,0 +1,68 @@
+import RequestEngine from '../../../netApi/requestEngine.js';
+var Promise = require('../../../libs/es6-promise.js').Promise;
+var config = require('../../../config.js');
+var netData = {
+  loadPackage(city, custid, tvCardNumber, serviceID, qrKind) {
+    let that = this;
+    return new Promise((resolve, reject) => {
+      new RequestEngine().request(config.querySalesList, { city, custid, tvCardNumber, serviceID, qrKind }, { callBy: that, method: that.loadPackage, params: [city, custid, tvCardNumber, serviceID, qrKind] }, (success) => {
+        resolve(success);
+      }, (faild) => {
+        reject(faild);
+      });
+    })
+  },
+  loadCurrentPackageInfo(city, custid, tvCardNumber, serviceID, qrKind) {
+    let that = this;
+    return new Promise((resolve, reject) => {
+      new RequestEngine().request(config.doQueServSalesPkgInfo, { city, custid, tvCardNumber, serviceID, qrKind }, { callBy: that, method: that.loadCurrentPackageInfo, params: [city, custid, tvCardNumber, serviceID, qrKind] }, (success) => {
+        resolve(success);
+      }, (faild) => {
+        reject(faild);
+      });
+    })
+  },
+  loadToast(qrid, city) {
+    let that = this;
+    return new Promise((resolve,reject)=>{
+      new RequestEngine().request(config.baseTrySee + `?qrid=${qrid}&city=${city}`, {}, { callBy: that, method: that.loadToast, params: [qrid, city] }, (success) => {
+        resolve(success);
+      }, (faild) => {
+        reject(faild);
+      });
+    })
+
+  },
+  loadCurrentPackageInfo(city, custid, tvCardNumber, serviceID, qrKind) {
+    let that = this;
+    return new Promise((resolve, reject) => {
+      new RequestEngine().request(config.doQueServSalesPkgInfo, { city, custid, tvCardNumber, serviceID, qrKind }, { callBy: that, method: that.loadCurrentPackageInfo, params: [city, custid, tvCardNumber, serviceID, qrKind] }, (success) => {
+        resolve(success);
+      }, (faild) => {
+        reject(faild);
+      });
+    })
+  },
+  loadCards(custid, tvCardNum, serviceID) {
+    let that = this;
+    return new Promise((resolve, reject) => {
+      new RequestEngine().request(config.queryOrderKeyno, { custid: custid }, { callBy: that, method: that.loadCards, params: [custid, tvCardNum, serviceID] }, (success) => {
+        resolve(success);
+      }, (faild) => {
+        reject(faild);
+      });
+    })
+
+  },
+  pay(param) {
+    let that = this;
+    return new Promise((resolve, reject) => {
+      new RequestEngine().request(config.wxPay, param, { callBy: that, method: that.pay, params: [] }, (success) => {
+        resolve(success);
+      }, (faild) => {
+        reject(faild);
+      });
+    })
+  },
+}
+module.exports = netData;
