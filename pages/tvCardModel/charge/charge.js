5849665ca76777cf24b89783e0bfedc884051314
max_wang <405323395@qq.com>
2018-10-26 09:57:54 +0800

    项目进度

--- pages/tvCardModel/charge/charge.js
+++ pages/tvCardModel/charge/charge.js
@@ -70,6 +70,11 @@ Page({
       totlaMoney: target.detail.value
     })
   },
+  inputMoneyFocus:function(target){
+    this.setData({
+      totlaMoney: target.detail.value
+    })
+  },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
