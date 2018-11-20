410cdd36df9ef1f8deaf41414ce694ad3452a4be
max_wang <405323395@qq.com>
2018-11-20 16:02:59 +0800

    html到小程序传递参数去支付

--- pages/tvCardModel/pay/payMoney/pay.js
+++ pages/tvCardModel/pay/payMoney/pay.js
@@ -8,68 +8,22 @@ Page({
    * 页面的初始数据
    */
   data: {
-    package1:null,
-    package2:null,
-    totalMoney:0,
-    usedCoup: { discountPrice:0}
+
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    this.package1 = appInstance.package1
-    this.package2 = appInstance.package2
-    this.freshUi();
-    this.loadCoup()
-  },
-  //加在优惠券
-  loadCoup(){
-    let that = this;
-    netData.queryUsrCanUseCoupons(appInstance.cardInfo).then(value=>{
-      let coups = value;
-      coups.sort(function (m, n) {
-        return n.discountPrice - m.discountPrice
-      })
-      console.log('可用优惠券，倒序：',coups)
-      for (let item of coups) {
-        if (item.fullPrice <= this.data.totalMoney) {
-          let money = (this.data.totalMoney - item.discountPrice).toFixed(2)
-          that.setData({
-            usedCoup: item,
-            totalMoney: money
-          })
-          return
-        }
-      }
-    })  
-  },
-  freshUi(){
-    let money1 = 0;
-    let money2 = 0;
-    if (this.package1 && this.package2.prdType == 'a104') {
-      money1 = parseFloat(this.package1.price)
-    }
-    if(this.package2) {
-      money2 = parseFloat(this.package2.price)
-    }
-    let totalMoney = 0;
-    if (appInstance.currentPackageInfo.feesums < 0) {
-      totalMoney = (money1 + money2 - appInstance.currentPackageInfo.feesums).toFixed(2)
+    this.params = options.params
+    if(!this.params) {
+      return;
     } else {
-      totalMoney = (money1 + money2).toFixed(2)
+      this.params = JSON.parse(decodeURIComponent(this.params))
     }
-    this.setData({
-      package1: this.package1,
-      package2: this.package2,
-      currentPackageInfo: appInstance.currentPackageInfo,
-      totalMoney: totalMoney
-    })
+    this.pay()
+  },
   
-  },
-  money:function(e) {
-    return 10202;
-  },
   pay: function () {
     let that = this;
     if (that.isPaying) {
@@ -80,22 +34,7 @@ Page({
     }
     dataUtil.buriedPoint("订单支付页,立即支付", appInstance.cardInfo);
     that.isPaying = true;
-    let currentIndex = that.data.packageSelectIndex
-    let param = {
-      custid: appInstance.cardInfo.custid,
-      tvCardNumber: appInstance.cardInfo.tvCardNum,
-      addr: appInstance.cardInfo.addr,
-      custname: appInstance.cardInfo.custname,
-      mobile: appInstance.cardInfo.mobile,
-      city: appInstance.cardInfo.city,
-      serviceid: appInstance.cardInfo.serviceID,
-      custfess: appInstance.currentPackageInfo.feesums,
-      unit: this.initUnit(),//订购单位 0：天；1：月；2：年
-      salestype: this.initSalestype(), //类型 0订购产品;1营销方案订购
-      salescode: this.initSalescode(),//产品编码
-      userCouponId: this.data.usedCoup.userCouponId //优惠券
-    };
-    netData.pay(param).then(value => {
+    netData.pay(this.params).then(value => {
       wx.requestPayment({
         'timeStamp': value.timeStamp,
         'nonceStr': value.nonceStr,
@@ -110,12 +49,8 @@ Page({
         },
         'fail': function (res) {
           that.isPaying = false;
+          wx.navigateBack({})
           ///// 记录错误日志
-          that.showFeedbackPaper({
-            target: {
-              id: 'pay-canceled'
-            }
-          })
           let uploadNetApi = require('../../requestUtil/uploadNetApi.js')
           uploadNetApi.payFaild(res.errMsg, appInstance.cardInfo.city, appInstance.cardInfo.custid, appInstance.cardInfo.tvCardNum, appInstance.cardInfo.serviceID, appInstance.cardInfo.qrKind, that.package1.salescode)
           //////
@@ -126,7 +61,12 @@ Page({
       wx.showModal({
         title: "支付失败,如有疑问请拨打客服电话96516",
         showCancel: false,
-        confirmText: "取消"
+        confirmText: "取消",
+        success(res) {
+          if (res.confirm) {
+            wx.navigateBack({}) 
+          }
+        }
       })
       ///// 记录错误日志
       let uploadNetApi = require('../../requestUtil/uploadNetApi.js')
@@ -135,72 +75,7 @@ Page({
     });
 
   },
-  initUnit() {
-    let u1 = '';
-    let u2 = '';
-    if (this.package1 && this.package2.prdType == 'a104') {
-      u1 = this.package1.unit
-    }
-    if (this.package2) {
-      u2 = this.package2.unit
-    }
-    let total='';
-    if(u1&&u2) {
-      total = u1 + ',' + u2;
-    } else if(u1) {
-      total = u1;
-    } else if(u2) {
-      total = u2;
-    }
-    
-    return total;
-  },
-  initSalestype() {
-    let u1 = '';
-    let u2 = '';
-    if (this.package1 && this.package2.prdType == 'a104') {
-      u1 = this.package1.salestype
-    }
-    if (this.package2) {
-      u2 = this.package2.salestype
-    }
-    let total = '';
-    if (u1 && u2) {
-      total = u1 + ',' + u2;
-    } else if (u1) {
-      total = u1;
-    } else if (u2) {
-      total = u2;
-    }
-    return total;
-  },
-  initSalescode() {
-    let u1 = '';
-    let u2 = '';
-    if (this.package1 && this.package2.prdType == 'a104') {
-      u1 = this.package1.salescode
-    }
-    if (this.package2) {
-      u2 = this.package2.salescode
-    }
-    let total = '';
-    if (u1 && u2) {
-      total = u1 + ',' + u2;
-    } else if (u1) {
-      total = u1;
-    } else if (u2) {
-      total = u2;
-    }
-    return total;
-
-  },
-  //跳转到反馈界面
-  showFeedbackPaper(event) {
-    wx.navigateTo({
-      url: '/pages/tvCardModel/feedback/feedback?feedBackType=' + event.target.id,
-    })
-
-  },
+  
 
 
   /**
